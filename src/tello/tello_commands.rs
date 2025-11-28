ef82dee44c5ba541012569c51b344ca01a586009
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-11-28 19:23:51 +0000

    Changed name of bind in tello_commands to promote clarity.

--- src/tello/tello_commands.rs
+++ src/tello/tello_commands.rs
@@ -5,7 +5,7 @@ pub struct TelloCommands {
 }
 
 impl TelloCommands {
-    pub fn bind() -> std::io::Result<TelloCommands> {
+    pub fn bind_and_connect() -> std::io::Result<TelloCommands> {
         let socket = UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 0))?;
         socket.connect(SocketAddrV4::new(Ipv4Addr::new(192, 168, 10, 1), 8889))?;
         Ok(TelloCommands { socket })
