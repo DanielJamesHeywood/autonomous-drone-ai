3f417f94b9d8588ae9a7d296604fbc4c05affdc7
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-11-28 18:23:54 +0000

    Implemented constructors for the tello sockets.

--- src/tello/tello_state.rs
+++ src/tello/tello_state.rs
@@ -3,3 +3,10 @@ use std::net::*;
 pub struct TelloState {
     socket: UdpSocket,
 }
+
+impl TelloState {
+    pub fn bind() -> std::io::Result<TelloState> {
+        let socket = UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 8890))?;
+        Ok(TelloState { socket })
+    }
+}
