b2e73ac487d10715dee807955f0d40607343f89c
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-11-29 02:41:32 +0000

    Restructured bind and connect to bind and connect seperately.

--- src/tello/tello_commands.rs
+++ src/tello/tello_commands.rs
@@ -6,8 +6,19 @@ pub struct TelloCommands {
 
 impl TelloCommands {
     pub fn bind_and_connect() -> std::io::Result<TelloCommands> {
+        let commands = TelloCommands::bind()?;
+        commands.connect()?;
+        Ok(commands)
+    }
+
+    fn bind() -> std::io::Result<TelloCommands> {
         let socket = UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 0))?;
-        socket.connect(SocketAddrV4::new(Ipv4Addr::new(192, 168, 10, 1), 8889))?;
         Ok(TelloCommands { socket })
+    }
+
+    fn connect(&self) -> std::io::Result<()> {
+        self.socket
+            .connect(SocketAddrV4::new(Ipv4Addr::new(192, 168, 10, 1), 8889))?;
+        Ok(())
     }
 }
