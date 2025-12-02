a12f0c9c7ef4d17bc60e7e29d6257932951a6c68
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-02 00:46:29 +0000

    Implemented input loop in main.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use std::io;
+
+fn main() -> io::Result<()> {
+    let standard_input = io::stdin();
+    loop {
+        let mut buffer = String::new();
+        standard_input.read_line(&mut buffer)?;
+    }
+}
