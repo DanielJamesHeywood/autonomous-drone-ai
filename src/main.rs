6cecf18299f33fc32441b52b608198015883493d
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-02 14:33:31 +0000

    Changed style of error handling in main.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::io;
 
-fn main() -> io::Result<()> {
+fn main() {
     let standard_input = io::stdin();
     loop {
         let mut buffer = String::new();
-        standard_input.read_line(&mut buffer)?;
+        standard_input.read_line(&mut buffer).unwrap();
     }
 }
