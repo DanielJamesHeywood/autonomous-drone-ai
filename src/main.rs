287ec485cb01107b4116cd8fd28e784aea8e4a55
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-05 16:19:49 +0000

    Fixed formatting in main.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     let standard_input = io::stdin();
     loop {
         let mut buffer = String::new();
-        standard_input.read_line(&mut buffer).expect("Failed to read line from standard input");
+        standard_input
+            .read_line(&mut buffer)
+            .expect("Failed to read line from standard input");
     }
 }
