d8b7bbd1221ac07afc39b840366809d095baad33
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-05 16:18:53 +0000

    Nicer error message for reading lines.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     let standard_input = io::stdin();
     loop {
         let mut buffer = String::new();
-        standard_input.read_line(&mut buffer).unwrap();
+        standard_input.read_line(&mut buffer).expect("Failed to read line from standard input");
     }
 }
