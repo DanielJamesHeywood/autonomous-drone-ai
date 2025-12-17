90c16a50edc63ff2c3970ccd437f112c8eb4e1c9
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-17 15:59:15 +0000

    Declared local variable storing possible decompression session.

--- autonomous drone ai/H264Decoder.swift
+++ autonomous drone ai/H264Decoder.swift
@@ -1,5 +1,9 @@
+import VideoToolbox
 
 public class H264Decoder {
+    
+    @usableFromInline
+    internal var _decompressionSession: VTDecompressionSession?
     
     @inlinable
     public init() {}
