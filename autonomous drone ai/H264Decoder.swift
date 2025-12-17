5d9f49ee7011d9f6df9475251a53e3d9faf3990e
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-17 16:37:43 +0000

    Changed to implicit initializer.

--- autonomous drone ai/H264Decoder.swift
+++ autonomous drone ai/H264Decoder.swift
@@ -6,9 +6,6 @@ public class H264Decoder {
     internal var _decompressionSession: VTDecompressionSession?
     
     @inlinable
-    public init() {}
-    
-    @inlinable
     deinit {
         if let _decompressionSession {
             VTDecompressionSessionInvalidate(_decompressionSession)
