0282cd9f13d8be4a5059df12b7b9d471fe9ad088
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-20 00:54:56 +0000

    Created network connection to back UDP Clients.

--- autonomous drone ai/UDP/UDPClient.swift
+++ autonomous drone ai/UDP/UDPClient.swift
@@ -1,3 +1,12 @@
 import Network
 
-public class UDPClient {}
+public class UDPClient {
+    
+    @usableFromInline
+    internal let _connection: NetworkConnection<UDP>
+    
+    @inlinable
+    public init(for endpoint: NWEndpoint) {
+        _connection = NetworkConnection(to: endpoint, using: { UDP() })
+    }
+}
