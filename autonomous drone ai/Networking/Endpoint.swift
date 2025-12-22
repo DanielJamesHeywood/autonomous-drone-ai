ccce838276b70c3c74751b2fb2f26d553e270cac
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-22 00:56:56 +0000

    Prefer Host to NWEndpoint.Host

--- autonomous drone ai/Networking/Endpoint.swift
+++ autonomous drone ai/Networking/Endpoint.swift
@@ -6,7 +6,18 @@ public struct Endpoint {
     internal let _endpoint: NWEndpoint
     
     @inlinable
-    public init(host: NWEndpoint.Host, port: Port) {
-        _endpoint = .hostPort(host: host, port: NWEndpoint.Port(rawValue: port).unsafelyUnwrapped)
+    public init(host: Host, port: Port) {
+        switch host {
+        case let .ipv4(a, b, c, d):
+            _endpoint = .hostPort(
+                host: NWEndpoint.Host("\(a).\(b).\(c).\(d)"),
+                port: NWEndpoint.Port(rawValue: port).unsafelyUnwrapped
+            )
+        case let .ipv6(a, b, c, d, e, f):
+            _endpoint = .hostPort(
+                host: NWEndpoint.Host("\(a).\(b).\(c).\(d).\(e).\(f)"),
+                port: NWEndpoint.Port(rawValue: port).unsafelyUnwrapped
+            )
+        }
     }
 }
---
+++ autonomous drone ai/Networking/Host.swift
@@ -0,0 +1,5 @@
+
+public enum Host {
+    case ipv4(UInt8, UInt8, UInt8, UInt8)
+    case ipv6(UInt8, UInt8, UInt8, UInt8, UInt8, UInt8)
+}
