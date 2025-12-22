e665626032291c1dbf4a8c437694952aa2c38d4b
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-22 00:02:13 +0000

    Implemented Endpoint structure to create a nicer public interface for UDP.

--- autonomous drone ai/Networking/Endpoint.swift
+++ autonomous drone ai/Networking/Endpoint.swift
@@ -1,1 +1,12 @@
+import Network
 
+public struct Endpoint {
+    
+    @usableFromInline
+    internal let _endpoint: NWEndpoint
+    
+    @inlinable
+    public init(host: NWEndpoint.Host, port: NWEndpoint.Port) {
+        _endpoint = .hostPort(host: host, port: port)
+    }
+}
