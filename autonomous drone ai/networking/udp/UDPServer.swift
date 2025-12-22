90aeae88cd465b5ffa8971af6c258349cd50dfdf
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-22 19:23:23 +0000

    Implemented new connection handler for the udp server.

--- autonomous drone ai/networking/udp/UDPServer.swift
+++ autonomous drone ai/networking/udp/UDPServer.swift
@@ -8,6 +8,7 @@ public class UDPServer {
     @inlinable
     public init(on port: NWEndpoint.Port) throws {
         _listener = try NWListener(using: .udp, on: port)
+        _listener.newConnectionHandler = { connection in }
         _listener.start(queue: DispatchQueue(label: "udpserver.listener", qos: .utility))
     }
     
