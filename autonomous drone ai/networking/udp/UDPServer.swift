9e3876f678106e2534dce389490a5c3a6090b2a1
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-22 19:36:07 +0000

    Changed UDP server initialiser to take a Port instead of a NWEndpoint.Port.

--- autonomous drone ai/networking/udp/UDPServer.swift
+++ autonomous drone ai/networking/udp/UDPServer.swift
@@ -6,8 +6,8 @@ public class UDPServer {
     internal let _listener: NWListener
     
     @inlinable
-    public init(on port: NWEndpoint.Port) throws {
-        _listener = try NWListener(using: .udp, on: port)
+    public init(on port: Endpoint.Port) throws {
+        _listener = try NWListener(using: .udp, on: port._convertToNWEndpointPort())
         _listener.newConnectionHandler = { connection in }
         _listener.start(queue: DispatchQueue(label: "udpserver.listener", qos: .utility))
     }
