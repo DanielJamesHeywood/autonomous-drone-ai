4abc3a63e32ebe9c097eebbe064fb11604049184
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-27 16:49:31 +0000

    Converted file descriptor to strong type.

--- autonomous drone ai/tello/TelloVideoStream.swift
+++ autonomous drone ai/tello/TelloVideoStream.swift
@@ -3,9 +3,13 @@ import System
 
 public class TelloVideoStream {
     
+    @usableFromInline
+    internal let _socketDescriptor: FileDescriptor
+    
     @inlinable
     public init() throws {
         let socketDescriptor = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
         guard socketDescriptor != -1 else { throw Errno(rawValue: errno) }
+        _socketDescriptor = FileDescriptor(rawValue: socketDescriptor)
     }
 }
