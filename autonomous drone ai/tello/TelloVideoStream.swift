ca8ef2a2f83bf77e9764e5db9d0c398f0a73bec6
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-27 15:11:52 +0000

    Throw error if call to socket was unsuccessful.

--- autonomous drone ai/tello/TelloVideoStream.swift
+++ autonomous drone ai/tello/TelloVideoStream.swift
@@ -1,9 +1,11 @@
 import Darwin
+import System
 
 public class TelloVideoStream {
     
     @inlinable
-    public init() {
+    public init() throws {
         let socketDescriptor = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        guard socketDescriptor != -1 else { throw Errno(rawValue: errno) }
     }
 }
