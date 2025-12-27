b2fd657f67bd4c9ba8b04a44727e1e0fd9bf4f2b
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-27 21:43:56 +0000

    Registered state update handler.

--- autonomous drone ai/tello/TelloVideoStream.swift
+++ autonomous drone ai/tello/TelloVideoStream.swift
@@ -8,6 +8,7 @@ public class TelloVideoStream {
     @inlinable
     public init() throws {
         _listener = try NWListener(using: .udp, on: 11111)
+        _listener.stateUpdateHandler = { _ in }
         _listener.newConnectionHandler = { _ in }
         _listener.start(queue: DispatchQueue(label: "tello.videostream.listener", qos: .utility))
     }
