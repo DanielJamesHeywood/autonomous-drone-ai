8032e517a514acfc5c29b57e4ea1ced0d0afe4fc
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-30 21:11:28 +0000

    Declared to-be AsyncStreams for State and VideoStream.

---
+++ autonomous drone ai/Tello.swift
@@ -0,0 +1,29 @@
+import Network
+
+public let state = State(_empty: ())
+
+public class State {
+    
+    @usableFromInline
+    internal let _connection = NetworkConnection(
+        to: .hostPort(host: "192.168.10.1", port: .any),
+        using: .parameters { UDP() } .localPort(8890)
+    )
+    
+    @inlinable
+    internal init(_empty: ()) {}
+}
+
+public let videoStream = VideoStream(_empty: ())
+
+public class VideoStream {
+    
+    @usableFromInline
+    internal let _connection = NetworkConnection(
+        to: .hostPort(host: "192.168.10.1", port: .any),
+        using: .parameters { UDP() } .localPort(11111)
+    )
+    
+    @inlinable
+    internal init(_empty: ()) {}
+}
