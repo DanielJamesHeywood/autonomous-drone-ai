154b7f2481e27a9331d80a12ca9d4ae8e610ca27
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-30 21:49:08 +0000

    Wrapped State and Video Stream in a Tello class.

--- autonomous drone ai/Tello.swift
+++ autonomous drone ai/Tello.swift
@@ -1,33 +1,37 @@
 import Network
 
-public struct State {
+public class Tello {
     
-    public struct Iterator {}
+    public struct State {
+        
+        public struct Iterator {}
+        
+        @usableFromInline
+        internal let _connection = NetworkConnection(
+            to: .hostPort(host: "192.168.10.1", port: .any),
+            using: .parameters { UDP() } .localPort(8890)
+        )
+        
+        @inlinable
+        internal init(_empty: ()) {}
+    }
     
-    @usableFromInline
-    internal let _connection = NetworkConnection(
-        to: .hostPort(host: "192.168.10.1", port: .any),
-        using: .parameters { UDP() } .localPort(8890)
-    )
+    public struct VideoStream {
+        
+        public struct Iterator {}
+        
+        @usableFromInline
+        internal let _connection = NetworkConnection(
+            to: .hostPort(host: "192.168.10.1", port: .any),
+            using: .parameters { UDP() } .localPort(11111)
+        )
+        
+        @inlinable
+        internal init(_empty: ()) {}
+    }
     
-    @inlinable
-    internal init(_empty: ()) {}
+    public let state = State(_empty: ())
+    
+    public let videoStream = VideoStream(_empty: ())
+    
 }
-
-public struct VideoStream {
-    
-    public struct Iterator {}
-    
-    @usableFromInline
-    internal let _connection = NetworkConnection(
-        to: .hostPort(host: "192.168.10.1", port: .any),
-        using: .parameters { UDP() } .localPort(11111)
-    )
-    
-    @inlinable
-    internal init(_empty: ()) {}
-}
-
-public let state = State(_empty: ())
-
-public let videoStream = VideoStream(_empty: ())
