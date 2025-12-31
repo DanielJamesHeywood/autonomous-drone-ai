19c3826f3c8936f19de3827941eb7acc466e8373
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-31 21:07:58 +0000

    Declared method for enabling video stream.

--- autonomous drone ai/Tello.swift
+++ autonomous drone ai/Tello.swift
@@ -68,6 +68,19 @@ public class Tello {
             throw TelloError.receivedInvalidResponse
         }
     }
+    
+    @inlinable
+    public func streamOn() async throws {
+        try await _connection.send("streamon".data(using: .utf8).unsafelyUnwrapped)
+        switch try await _connection.receive().content {
+        case "ok".data(using: .utf8).unsafelyUnwrapped:
+            break
+        case "error".data(using: .utf8).unsafelyUnwrapped:
+            throw TelloError.receivedError
+        default:
+            throw TelloError.receivedInvalidResponse
+        }
+    }
 }
 
 public let tello = Tello(_empty: ())
