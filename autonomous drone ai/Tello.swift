226d51111c5f212be98ef86e564068e6e8e15fa2
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-31 21:00:40 +0000

    Declared function for handling takeoff.

--- autonomous drone ai/Tello.swift
+++ autonomous drone ai/Tello.swift
@@ -39,6 +39,19 @@ public class Tello {
             throw TelloError.receivedInvalidResponse
         }
     }
+    
+    @inlinable
+    public func takeoff() async throws {
+        try await _connection.send("takeoff".data(using: .utf8).unsafelyUnwrapped)
+        switch try await _connection.receive().content {
+        case "ok".data(using: .utf8).unsafelyUnwrapped:
+            break
+        case "error".data(using: .utf8).unsafelyUnwrapped:
+            throw TelloError.receivedError
+        default:
+            throw TelloError.receivedInvalidResponse
+        }
+    }
 }
 
 public let tello = Tello(_empty: ())
