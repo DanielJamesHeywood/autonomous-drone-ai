b62971430df347b2ec4c78f444498263732e7ac4
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-31 20:56:17 +0000

    Removed State and VideoStream as they should just be AsyncThrowingStreams.

--- autonomous drone ai/Tello.swift
+++ autonomous drone ai/Tello.swift
@@ -9,22 +9,6 @@ public enum TelloError: Error {
 
 public class Tello {
     
-    public struct State {
-        
-        public struct Iterator {}
-        
-        @inlinable
-        internal init(_empty: ()) {}
-    }
-    
-    public struct VideoStream {
-        
-        public struct Iterator {}
-        
-        @inlinable
-        internal init(_empty: ()) {}
-    }
-    
     @usableFromInline
     internal let _connection = NetworkConnection(to: .hostPort(host: "192.168.10.1", port: 8889), using: { UDP() })
     
@@ -55,10 +39,6 @@ public class Tello {
             throw TelloError.receivedInvalidResponse
         }
     }
-    
-    public let state = State(_empty: ())
-    
-    public let videoStream = VideoStream(_empty: ())
 }
 
 public let tello = Tello(_empty: ())
