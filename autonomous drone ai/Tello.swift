2257a3a84d8d8ceb61a48f7031d1bf73e63d6174
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-12-31 21:36:38 +0000

    Implemented stop method for tello.

--- autonomous drone ai/Tello.swift
+++ autonomous drone ai/Tello.swift
@@ -107,6 +107,19 @@ public class Tello {
             throw TelloError.receivedInvalidResponse
         }
     }
+    
+    @inlinable
+    public func stop() async throws {
+        try await _connection.send("stop".data(using: .utf8).unsafelyUnwrapped)
+        switch try await _connection.receive().content {
+        case "ok".data(using: .utf8).unsafelyUnwrapped:
+            break
+        case "error".data(using: .utf8).unsafelyUnwrapped:
+            throw TelloError.receivedError
+        default:
+            throw TelloError.receivedInvalidResponse
+        }
+    }
 }
 
 public let tello = Tello(_empty: ())
