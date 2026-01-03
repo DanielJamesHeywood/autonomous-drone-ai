e03f0cc9b9cf4f4e934a57ad6bec3bc4b2d4599a
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-03 20:49:40 +0000

    Initial Commit

---
+++ autonomous drone ai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  autonomous drone ai
+//
+//  Created by Daniel James Heywood on 03/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ autonomous drone ai/autonomous_drone_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  autonomous_drone_aiApp.swift
+//  autonomous drone ai
+//
+//  Created by Daniel James Heywood on 03/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct autonomous_drone_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
