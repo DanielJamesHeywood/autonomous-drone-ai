fe70cf6091db82650eaeef396e9b0ae1a9a6c8fa
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-03 22:29:21 +0000

    Fixed casing of the window id.

--- autonomous drone ai/AutonomousDroneAIApp.swift
+++ autonomous drone ai/AutonomousDroneAIApp.swift
@@ -3,5 +3,5 @@ import SwiftUI
 @main
 internal struct AutonomousDroneAIApp: App {
     
-    internal var body: some Scene { Window("Autonomous Drone AI", id: "autonomous drone ai") { ContentView() } }
+    internal var body: some Scene { Window("Autonomous Drone AI", id: "autonomousDroneAI") { ContentView() } }
 }
