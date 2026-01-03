fb0162aea38613a2db5019f4aa6cbc062c7b5bbd
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-03 21:52:20 +0000

    Switched from WindowGroup to Window as our application only requires a single window.

--- autonomous drone ai/AutonomousDroneAIApp.swift
+++ autonomous drone ai/AutonomousDroneAIApp.swift
@@ -3,5 +3,5 @@ import SwiftUI
 @main
 internal struct AutonomousDroneAIApp: App {
     
-    internal var body: some Scene { WindowGroup { ContentView() } }
+    internal var body: some Scene { Window("", id: "autonomous drone ai") { ContentView() } }
 }
