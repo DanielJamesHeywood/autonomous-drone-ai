062d28a2a109ee3fe0854606ddf78e4feaa6f93c
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-04 04:42:09 +0000

    Create and return a view seperately so we can configure it properly.

--- autonomous drone ai/ContentView.swift
+++ autonomous drone ai/ContentView.swift
@@ -3,7 +3,10 @@ import MetalKit
 
 struct ContentView: NSViewRepresentable {
 
-    func makeNSView(context: Context) -> MTKView { MTKView() }
+    func makeNSView(context: Context) -> MTKView {
+        let view = MTKView()
+        return view
+    }
 
     func updateNSView(_ nsView: MTKView, context: Context) {}
 }
