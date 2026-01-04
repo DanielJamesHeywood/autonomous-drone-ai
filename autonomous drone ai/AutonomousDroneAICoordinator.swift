22136d9df0ea7b34f75744752527bcc615017d54
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-04 22:30:24 +0000

    Implemented explicit overriding initializer for the coordinator.

--- autonomous drone ai/AutonomousDroneAICoordinator.swift
+++ autonomous drone ai/AutonomousDroneAICoordinator.swift
@@ -2,6 +2,8 @@ import MetalKit
 
 class AutonomousDroneAICoordinator: NSObject, MTKViewDelegate {
 
+    override init() {}
+
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
 
     func draw(in view: MTKView) {}
