dfa90b5633dc35e199173065ffb0f3d9c8900afd
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-04 22:59:11 +0000

    It is almost guaranteed that we will need the device again in the coordinator initializer.

--- autonomous drone ai/AutonomousDroneAICoordinator.swift
+++ autonomous drone ai/AutonomousDroneAICoordinator.swift
@@ -2,7 +2,12 @@ import MetalKit
 
 class AutonomousDroneAICoordinator: NSObject, MTKViewDelegate {
 
-    let commandQueue = MTLCreateSystemDefaultDevice()?.makeMTL4CommandQueue()
+    let commandQueue: MTL4CommandQueue?
+
+    override init() {
+        let device = MTLCreateSystemDefaultDevice()
+        commandQueue = device?.makeMTL4CommandQueue()
+    }
 
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
 
