12e919f172d946aa7af30f7c9ae6f3b848ff2c9a
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-04 04:14:58 +0000

    Removed unecessary whitespace.

--- autonomous drone ai/ContentView.swift
+++ autonomous drone ai/ContentView.swift
@@ -2,8 +2,8 @@ import SwiftUI
 import MetalKit
 
 struct ContentView: NSViewRepresentable {
-    
+
     func makeNSView(context: Context) -> MTKView { MTKView() }
-    
+
     func updateNSView(_ nsView: MTKView, context: Context) {}
 }
