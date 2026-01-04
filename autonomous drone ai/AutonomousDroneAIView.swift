666159237188f4dcbc02f86aecaac53dec5ff8f2
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-04 12:43:36 +0000

    Implemented AutonomousDroneAIViewDelegate.

--- autonomous drone ai/AutonomousDroneAIView.swift
+++ autonomous drone ai/AutonomousDroneAIView.swift
@@ -5,8 +5,11 @@ struct AutonomousDroneAIView: NSViewRepresentable {
 
     func makeNSView(context: Context) -> MTKView {
         let view = MTKView()
+        view.delegate = context.coordinator
         return view
     }
 
     func updateNSView(_ view: MTKView, context: Context) {}
+    
+    func makeCoordinator() -> AutonomousDroneAIViewDelegate { AutonomousDroneAIViewDelegate() }
 }
---
+++ autonomous drone ai/AutonomousDroneAIViewDelegate.swift
@@ -0,0 +1,8 @@
+import MetalKit
+
+class AutonomousDroneAIViewDelegate: NSObject, MTKViewDelegate {
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
+    
+    func draw(in view: MTKView) {}
+}
