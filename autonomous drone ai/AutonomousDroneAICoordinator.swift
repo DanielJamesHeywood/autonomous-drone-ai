9d5560dc5341a115c1564a93e8be49603117f019
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-05 16:09:08 +0000

    Moved device declaration inside initializer.

--- autonomous drone ai/AutonomousDroneAICoordinator.swift
+++ autonomous drone ai/AutonomousDroneAICoordinator.swift
@@ -2,8 +2,6 @@ import MetalKit
 
 class AutonomousDroneAICoordinator: NSObject, MTKViewDelegate {
 
-    let device = MTLCreateSystemDefaultDevice()
-
     let commandQueue: MTL4CommandQueue?
 
     let commandBuffer: MTL4CommandBuffer?
@@ -11,6 +9,7 @@ class AutonomousDroneAICoordinator: NSObject, MTKViewDelegate {
     let commandAllocator: MTL4CommandAllocator?
 
     override init() {
+        let device = MTLCreateSystemDefaultDevice()
         commandQueue = device?.makeMTL4CommandQueue()
         commandBuffer = device?.makeCommandBuffer()
         commandAllocator = device?.makeCommandAllocator()
@@ -19,7 +18,7 @@ class AutonomousDroneAICoordinator: NSObject, MTKViewDelegate {
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
 
     func draw(in view: MTKView) {
-        guard let device, let commandQueue, let commandBuffer, let commandAllocator else { return }
+        guard let commandQueue, let commandBuffer, let commandAllocator else { return }
         commandBuffer.beginCommandBuffer(allocator: commandAllocator)
         commandBuffer.endCommandBuffer()
         commandQueue.commit([commandBuffer])
