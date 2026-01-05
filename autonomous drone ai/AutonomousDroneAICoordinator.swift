33f42575c5fcd4b5b43b48c7bb2f010499e40fe3
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-05 16:06:26 +0000

    Reuse command buffer instead of allocating a new one for each draw command.

--- autonomous drone ai/AutonomousDroneAICoordinator.swift
+++ autonomous drone ai/AutonomousDroneAICoordinator.swift
@@ -6,18 +6,20 @@ class AutonomousDroneAICoordinator: NSObject, MTKViewDelegate {
 
     let commandQueue: MTL4CommandQueue?
 
+    let commandBuffer: MTL4CommandBuffer?
+
     let commandAllocator: MTL4CommandAllocator?
 
     override init() {
         commandQueue = device?.makeMTL4CommandQueue()
+        commandBuffer = device?.makeCommandBuffer()
         commandAllocator = device?.makeCommandAllocator()
     }
 
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
 
     func draw(in view: MTKView) {
-        guard let device, let commandQueue, let commandAllocator else { return }
-        guard let commandBuffer = device.makeCommandBuffer() else { return }
+        guard let device, let commandQueue, let commandBuffer, let commandAllocator else { return }
         commandBuffer.beginCommandBuffer(allocator: commandAllocator)
         commandBuffer.endCommandBuffer()
         commandQueue.commit([commandBuffer])
