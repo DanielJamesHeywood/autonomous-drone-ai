9c6a89cf02e8662bb529bb21af4bb36404fb2ddc
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-05 22:29:58 +0000

    Switched from independent optional values to a non-independent tuple for state.

--- autonomous drone ai/AutonomousDroneAICoordinator.swift
+++ autonomous drone ai/AutonomousDroneAICoordinator.swift
@@ -2,33 +2,45 @@ import MetalKit
 
 class AutonomousDroneAICoordinator: NSObject, MTKViewDelegate {
 
-    let commandQueue: MTL4CommandQueue?
-
-    let commandBuffer: MTL4CommandBuffer?
-
-    let commandAllocator: MTL4CommandAllocator?
-
-    let sharedEvent: MTLSharedEvent?
+    let state: (
+        commandQueue: MTL4CommandQueue,
+        commandBuffer: MTL4CommandBuffer,
+        commandAllocator: MTL4CommandAllocator,
+        sharedEvent: MTLSharedEvent
+    )?
 
     override init() {
         let device = MTLCreateSystemDefaultDevice()
-        commandQueue = device?.makeMTL4CommandQueue()
-        commandBuffer = device?.makeCommandBuffer()
-        commandAllocator = device?.makeCommandAllocator()
-        sharedEvent = device?.makeSharedEvent()
+        guard let commandQueue = device?.makeMTL4CommandQueue() else {
+            state = nil
+            return
+        }
+        guard let commandBuffer = device?.makeCommandBuffer() else {
+            state = nil
+            return
+        }
+        guard let commandAllocator = device?.makeCommandAllocator() else {
+            state = nil
+            return
+        }
+        guard let sharedEvent = device?.makeSharedEvent() else {
+            state = nil
+            return
+        }
+        state = (commandQueue, commandBuffer, commandAllocator, sharedEvent)
     }
 
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
 
     func draw(in view: MTKView) {
-        guard let commandQueue, let commandBuffer, let commandAllocator, let sharedEvent else { return }
+        guard let state else { return }
         guard let drawable = view.currentDrawable else { return }
-        commandAllocator.reset()
-        commandBuffer.beginCommandBuffer(allocator: commandAllocator)
-        commandBuffer.endCommandBuffer()
-        commandQueue.waitForDrawable(drawable)
-        commandQueue.commit([commandBuffer])
-        commandQueue.signalDrawable(drawable)
+        state.commandAllocator.reset()
+        state.commandBuffer.beginCommandBuffer(allocator: state.commandAllocator)
+        state.commandBuffer.endCommandBuffer()
+        state.commandQueue.waitForDrawable(drawable)
+        state.commandQueue.commit([state.commandBuffer])
+        state.commandQueue.signalDrawable(drawable)
         drawable.present()
     }
 }
