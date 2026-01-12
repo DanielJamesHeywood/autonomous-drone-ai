37171e953e692211746f2df5378c6cbcc490f49b
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-12 13:06:41 +0000

    Create and start connection to Tello asynchronously.

--- autonomous drone ai/AutonomousDroneAIApp.swift
+++ autonomous drone ai/AutonomousDroneAIApp.swift
@@ -1,7 +1,29 @@
+import Network
 import SwiftUI
 
 @main
 struct AutonomousDroneAIApp: App {
+    
+    enum Error: Swift.Error {
+        case receivedErrorResponse
+        case receivedInvalidResponse
+    }
+    
+    let task = Task(
+        priority: .utility,
+        operation: {
+            let connection = NetworkConnection(to: .hostPort(host: "192.168.10.1", port: 8889), using: { UDP() })
+            try await connection.send("command".data(using: .utf8).unsafelyUnwrapped)
+            switch try await connection.receive().content {
+            case "ok".data(using: .utf8).unsafelyUnwrapped:
+                break
+            case "error".data(using: .utf8).unsafelyUnwrapped:
+                throw Error.receivedErrorResponse
+            default:
+                throw Error.receivedInvalidResponse
+            }
+        }
+    )
     
     var body: some Scene {
         Window("Autonomous Drone AI", id: "autonomousDroneAI", content: { AutonomousDroneAIView() })
