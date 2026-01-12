2777ff1a271cfa346e113e39da91a34633af3fa1
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-12 13:12:27 +0000

    Wrapped Tello interface in a Tello class.

--- autonomous drone ai/AutonomousDroneAIApp.swift
+++ autonomous drone ai/AutonomousDroneAIApp.swift
@@ -12,16 +12,7 @@ struct AutonomousDroneAIApp: App {
     let task = Task(
         priority: .utility,
         operation: {
-            let connection = NetworkConnection(to: .hostPort(host: "192.168.10.1", port: 8889), using: { UDP() })
-            try await connection.send("command".data(using: .utf8).unsafelyUnwrapped)
-            switch try await connection.receive().content {
-            case "ok".data(using: .utf8).unsafelyUnwrapped:
-                break
-            case "error".data(using: .utf8).unsafelyUnwrapped:
-                throw Error.receivedErrorResponse
-            default:
-                throw Error.receivedInvalidResponse
-            }
+            try await tello.command()
         }
     )
     
---
+++ autonomous drone ai/Tello.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Network
+
+class Tello {
+    
+    public enum Error: Swift.Error {
+        case receivedErrorResponse
+        case receivedInvalidResponse
+    }
+    
+    let connection = NetworkConnection(to: .hostPort(host: "192.168.10.1", port: 8889), using: { UDP() })
+    
+    func command() async throws {
+        try await connection.send("command".data(using: .utf8).unsafelyUnwrapped)
+        switch try await connection.receive().content {
+        case "ok".data(using: .utf8).unsafelyUnwrapped:
+            break
+        case "error".data(using: .utf8).unsafelyUnwrapped:
+            throw Error.receivedErrorResponse
+        default:
+            throw Error.receivedInvalidResponse
+        }
+    }
+}
+
+let tello = Tello()
