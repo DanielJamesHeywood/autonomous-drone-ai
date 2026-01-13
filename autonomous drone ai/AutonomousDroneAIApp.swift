37c80db6adaf810b6b246393591ebd59b8e0de18
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-13 18:18:21 +0000

    Indentation changes to App.

--- autonomous drone ai/AutonomousDroneAIApp.swift
+++ autonomous drone ai/AutonomousDroneAIApp.swift
@@ -11,7 +11,11 @@ struct AutonomousDroneAIApp: App {
                 AutonomousDroneAIView().task(priority: .utility) {
                     let tello = Tello()
                     try! await tello.command()
-                } .task(priority: .utility) {} .task(priority: .utility) {}
+                } .task(priority: .utility) {
+                    
+                } .task(priority: .utility) {
+                    
+                }
             }
         )
     }
