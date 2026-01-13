4187427aa2e4bf7b65140a718a356e9d4460d808
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-13 16:06:52 +0000

    Removed unecessary Context class from the coordinator.

--- autonomous drone ai/AutonomousDroneAICoordinator.swift
+++ autonomous drone ai/AutonomousDroneAICoordinator.swift
@@ -2,65 +2,60 @@ import MetalKit
 
 class AutonomousDroneAICoordinator: NSObject, MTKViewDelegate {
     
-    class Context {
-        
-        let commandQueue: MTL4CommandQueue
-        
-        let commandBuffer: MTL4CommandBuffer
-        
-        let commandAllocators: [MTL4CommandAllocator]
-        
-        let renderPipelineState: MTLRenderPipelineState
-        
-        let depthStencilState: MTLDepthStencilState
-        
-        let sharedEvent: MTLSharedEvent
-        
-        var frameNumber = 0 as UInt64
-        
-        init() {
-            let device = MTLCreateSystemDefaultDevice()!
-            self.commandQueue = device.makeMTL4CommandQueue()!
-            self.commandBuffer = device.makeCommandBuffer()!
-            var commandAllocators = [] as [MTL4CommandAllocator]
-            repeat {
-                commandAllocators.append(device.makeCommandAllocator()!)
-            } while commandAllocators.count < 3
-            self.commandAllocators = commandAllocators
-            self.renderPipelineState = {
-                fatalError()
-            }()
-            let depthStencilDescriptor = MTLDepthStencilDescriptor()
-            depthStencilDescriptor.depthCompareFunction = .less
-            self.depthStencilState = device.makeDepthStencilState(descriptor: depthStencilDescriptor)!
-            self.sharedEvent = device.makeSharedEvent()!
-        }
-    }
+    let commandQueue: MTL4CommandQueue
     
-    let context = Context()
+    let commandBuffer: MTL4CommandBuffer
+    
+    let commandAllocators: [MTL4CommandAllocator]
+    
+    let renderPipelineState: MTLRenderPipelineState
+    
+    let depthStencilState: MTLDepthStencilState
+    
+    let sharedEvent: MTLSharedEvent
+    
+    var frameNumber = 0 as UInt64
+    
+    override init() {
+        let device = MTLCreateSystemDefaultDevice()!
+        self.commandQueue = device.makeMTL4CommandQueue()!
+        self.commandBuffer = device.makeCommandBuffer()!
+        var commandAllocators = [] as [MTL4CommandAllocator]
+        repeat {
+            commandAllocators.append(device.makeCommandAllocator()!)
+        } while commandAllocators.count < 3
+        self.commandAllocators = commandAllocators
+        self.renderPipelineState = {
+            fatalError()
+        }()
+        let depthStencilDescriptor = MTLDepthStencilDescriptor()
+        depthStencilDescriptor.depthCompareFunction = .less
+        self.depthStencilState = device.makeDepthStencilState(descriptor: depthStencilDescriptor)!
+        self.sharedEvent = device.makeSharedEvent()!
+    }
     
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
     
     func draw(in view: MTKView) {
         guard let renderPassDescriptor = view.currentMTL4RenderPassDescriptor else { return }
         guard let drawable = view.currentDrawable else { return }
-        let commandAllocator = context.commandAllocators[Int(context.frameNumber % 3)]
-        if context.frameNumber >= 3 {
-            guard context.sharedEvent.wait(untilSignaledValue: context.frameNumber - 3, timeoutMS: 1000) else { return }
+        let commandAllocator = commandAllocators[Int(frameNumber % 3)]
+        if frameNumber >= 3 {
+            guard sharedEvent.wait(untilSignaledValue: frameNumber - 3, timeoutMS: 1000) else { return }
             commandAllocator.reset()
         }
-        context.commandBuffer.beginCommandBuffer(allocator: commandAllocator)
-        guard let renderCommandEncoder = context.commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else { return }
-        renderCommandEncoder.setRenderPipelineState(context.renderPipelineState)
-        renderCommandEncoder.setDepthStencilState(context.depthStencilState)
+        commandBuffer.beginCommandBuffer(allocator: commandAllocator)
+        guard let renderCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else { return }
+        renderCommandEncoder.setRenderPipelineState(renderPipelineState)
+        renderCommandEncoder.setDepthStencilState(depthStencilState)
         renderCommandEncoder.setViewport(MTLViewport(originX: 0, originY: 0, width: view.drawableSize.width, height: view.drawableSize.height, znear: 0, zfar: 1))
         renderCommandEncoder.endEncoding()
-        context.commandBuffer.endCommandBuffer()
-        context.commandQueue.waitForDrawable(drawable)
-        context.commandQueue.commit([context.commandBuffer])
-        context.commandQueue.signalDrawable(drawable)
-        context.commandQueue.signalEvent(context.sharedEvent, value: context.frameNumber)
-        context.frameNumber += 1
+        commandBuffer.endCommandBuffer()
+        commandQueue.waitForDrawable(drawable)
+        commandQueue.commit([commandBuffer])
+        commandQueue.signalDrawable(drawable)
+        commandQueue.signalEvent(sharedEvent, value: frameNumber)
+        frameNumber += 1
         drawable.present()
     }
 }
