a6b805ee2530dc857dbf21d50a647a312a1656ff
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-14 21:14:59 +0000

    Declared Iterators for State and VideoStream.

--- autonomous drone ai/TelloState.swift
+++ autonomous drone ai/TelloState.swift
@@ -2,6 +2,8 @@ import Network
 
 class TelloState {
     
+    class Iterator {}
+    
     let _connection = NetworkConnection(
         to: .hostPort(host: "192.168.10.1", port: .any),
         using: .parameters { UDP() } .localPort(8890)
--- autonomous drone ai/TelloVideoStream.swift
+++ autonomous drone ai/TelloVideoStream.swift
@@ -2,6 +2,8 @@ import Network
 
 class TelloVideoStream {
     
+    class Iterator {}
+    
     let _connection = NetworkConnection(
         to: .hostPort(host: "192.168.10.1", port: .any),
         using: .parameters { UDP() } .localPort(11111)
