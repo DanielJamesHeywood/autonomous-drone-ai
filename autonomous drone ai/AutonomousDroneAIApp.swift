f367cf512063d990b315fdf482f9fec23071a4d1
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-17 23:24:52 +0000

    Implemented basic controller handling.

--- autonomous drone ai/AutonomousDroneAIApp.swift
+++ autonomous drone ai/AutonomousDroneAIApp.swift
@@ -11,7 +11,27 @@ struct AutonomousDroneAIApp: App {
             id: "autonomousDroneAI",
             content: {
                 AutonomousDroneAIView().task(priority: .utility) {
-                    await GCController.startWirelessControllerDiscovery()
+                    for await notification in NotificationCenter.default.notifications(named: .GCControllerDidBecomeCurrent) {
+                        let controller = notification.object as! GCController
+                        guard let gamepad = controller.extendedGamepad else { continue }
+                        gamepad.buttonA.pressedChangedHandler = { _, _, pressed in
+                            guard pressed else { return }
+                        }
+                        gamepad.buttonB.pressedChangedHandler = { _, _, pressed in
+                            guard pressed else { return }
+                        }
+                        gamepad.leftThumbstick.valueChangedHandler = { _, xValue, yValue in }
+                        gamepad.rightThumbstick.valueChangedHandler = { _, xValue, yValue in }
+                    }
+                } .task(priority: .utility) {
+                    for await notification in NotificationCenter.default.notifications(named: .GCControllerDidStopBeingCurrent) {
+                        let controller = notification.object as! GCController
+                        guard let gamepad = controller.extendedGamepad else { continue }
+                        gamepad.buttonA.pressedChangedHandler = nil
+                        gamepad.buttonB.pressedChangedHandler = nil
+                        gamepad.leftThumbstick.valueChangedHandler = nil
+                        gamepad.rightThumbstick.valueChangedHandler = nil
+                    }
                 } .task(priority: .utility) {
                     let tello = Tello()
                     try! await tello.command()
