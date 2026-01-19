5b04d38491bd69e295bc538268f8b52e232b456d
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-19 17:15:01 +0000

    Moved logic outside of Tello Controller and into main app scoped tasks.

--- autonomous drone ai/AutonomousDroneAIApp.swift
+++ autonomous drone ai/AutonomousDroneAIApp.swift
@@ -12,7 +12,38 @@ struct AutonomousDroneAIApp: App {
             "Autonomous Drone AI",
             id: "autonomousDroneAI",
             content: {
-                AutonomousDroneAIView()
+                AutonomousDroneAIView().task(priority: .utility) {
+                    let tello = Tello()
+                    await _telloController.initializeTello(tello)
+                } .task(priority: .utility) {
+                    for await notification in NotificationCenter.default.notifications(named: .GCControllerDidBecomeCurrent) {
+                        let controller = notification.object as! GCController
+                        guard let gamepad = controller.extendedGamepad else { continue }
+                        gamepad.buttonA.pressedChangedHandler = { [self] _, _, pressed in
+                            guard pressed else { return }
+                            Task(
+                                operation: {
+                                    await _telloController.buttonAPressed()
+                                }
+                            )
+                        }
+                        gamepad.buttonB.pressedChangedHandler = { [self] _, _, pressed in
+                            guard pressed else { return }
+                            Task(
+                                operation: {
+                                    await _telloController.buttonBPressed()
+                                }
+                            )
+                        }
+                    }
+                } .task(priority: .utility) {
+                    for await notification in NotificationCenter.default.notifications(named: .GCControllerDidStopBeingCurrent) {
+                        let controller = notification.object as! GCController
+                        guard let gamepad = controller.extendedGamepad else { continue }
+                        gamepad.buttonA.pressedChangedHandler = nil
+                        gamepad.buttonB.pressedChangedHandler = nil
+                    }
+                }
             }
         )
     }
--- autonomous drone ai/TelloController.swift
+++ autonomous drone ai/TelloController.swift
@@ -2,22 +2,16 @@ import GameController
 
 actor TelloController {
     
-    var task1: Task<Void, Never>?
-    
-    var task2: Task<Void, Never>?
-    
-    var task3: Task<Void, Never>?
-    
     var _tello: Tello?
     
     var _isFlying = false
     
-    func _initializeTello(_ tello: Tello) {
+    func initializeTello(_ tello: Tello) {
         precondition(_tello == nil)
         _tello = tello
     }
     
-    func _buttonAPressed() async {
+    func buttonAPressed() async {
         guard let _tello else { return }
         do {
             try await _isFlying ? _tello.land() : _tello.takeoff()
@@ -25,63 +19,11 @@ actor TelloController {
         } catch {}
     }
     
-    func _buttonBPressed() async {
+    func buttonBPressed() async {
         guard let _tello, _isFlying else { return }
         do {
             try await _tello.emergency()
             _isFlying = false
         } catch {}
-    }
-    
-    init() {
-        task1 = Task(
-            priority: .utility,
-            operation: {
-                let tello = Tello()
-                await _initializeTello(tello)
-            }
-        )
-        task2 = Task(
-            priority: .utility,
-            operation: {
-                for await notification in NotificationCenter.default.notifications(named: .GCControllerDidBecomeCurrent) {
-                    let controller = notification.object as! GCController
-                    guard let gamepad = controller.extendedGamepad else { continue }
-                    gamepad.buttonA.pressedChangedHandler = { [self] _, _, pressed in
-                        guard pressed else { return }
-                        Task(
-                            operation: {
-                                await _buttonAPressed()
-                            }
-                        )
-                    }
-                    gamepad.buttonB.pressedChangedHandler = { [self] _, _, pressed in
-                        guard pressed else { return }
-                        Task(
-                            operation: {
-                                await _buttonBPressed()
-                            }
-                        )
-                    }
-                }
-            }
-        )
-        task3 = Task(
-            priority: .utility,
-            operation: {
-                for await notification in NotificationCenter.default.notifications(named: .GCControllerDidStopBeingCurrent) {
-                    let controller = notification.object as! GCController
-                    guard let gamepad = controller.extendedGamepad else { continue }
-                    gamepad.buttonA.pressedChangedHandler = nil
-                    gamepad.buttonB.pressedChangedHandler = nil
-                }
-            }
-        )
-    }
-    
-    deinit {
-        task1.unsafelyUnwrapped.cancel()
-        task2.unsafelyUnwrapped.cancel()
-        task3.unsafelyUnwrapped.cancel()
     }
 }
