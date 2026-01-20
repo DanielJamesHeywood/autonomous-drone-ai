8283f3dbe82ff14f9302c9282ca078f3b320d7eb
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-20 12:54:28 +0000

    Moved remaining logic from app into flight controller.

--- autonomous drone ai/AutonomousDroneAIApp.swift
+++ autonomous drone ai/AutonomousDroneAIApp.swift
@@ -1,5 +1,3 @@
-import Foundation
-import GameController
 import SwiftUI
 
 @main
@@ -15,23 +13,9 @@ struct AutonomousDroneAIApp: App {
                 AutonomousDroneAIView().task(priority: .utility) {
                     try? await _flightController.initializeTello()
                 } .task(priority: .utility) {
-                    for await notification in NotificationCenter.default.notifications(named: .GCControllerDidBecomeCurrent) {
-                        let controller = notification.object as! GCController
-                        guard let gamepad = controller.extendedGamepad else { continue }
-                        gamepad.buttonA.pressedChangedHandler = { _, _, pressed in
-                            guard pressed else { return }
-                        }
-                        gamepad.buttonB.pressedChangedHandler = { _, _, pressed in
-                            guard pressed else { return }
-                        }
-                    }
+                    await _flightController.handleControllerDidBecomeCurrentNotifications()
                 } .task(priority: .utility) {
-                    for await notification in NotificationCenter.default.notifications(named: .GCControllerDidStopBeingCurrent) {
-                        let controller = notification.object as! GCController
-                        guard let gamepad = controller.extendedGamepad else { continue }
-                        gamepad.buttonA.pressedChangedHandler = nil
-                        gamepad.buttonB.pressedChangedHandler = nil
-                    }
+                    await _flightController.handleControllerDidStopBeingCurrentNotifications()
                 }
             }
         )
--- autonomous drone ai/FlightController.swift
+++ autonomous drone ai/FlightController.swift
@@ -1,3 +1,5 @@
+import Foundation
+import GameController
 
 actor FlightController {
     
@@ -12,6 +14,28 @@ actor FlightController {
             try await tello.streamOn()
         }
         _tello = tello
+    }
+    
+    func handleControllerDidBecomeCurrentNotifications() async {
+        for await notification in NotificationCenter.default.notifications(named: .GCControllerDidBecomeCurrent) {
+            let controller = notification.object as! GCController
+            guard let gamepad = controller.extendedGamepad else { continue }
+            gamepad.buttonA.pressedChangedHandler = { _, _, pressed in
+                guard pressed else { return }
+            }
+            gamepad.buttonB.pressedChangedHandler = { _, _, pressed in
+                guard pressed else { return }
+            }
+        }
+    }
+    
+    func handleControllerDidStopBeingCurrentNotifications() async {
+        for await notification in NotificationCenter.default.notifications(named: .GCControllerDidStopBeingCurrent) {
+            let controller = notification.object as! GCController
+            guard let gamepad = controller.extendedGamepad else { continue }
+            gamepad.buttonA.pressedChangedHandler = nil
+            gamepad.buttonB.pressedChangedHandler = nil
+        }
     }
     
     func _retryingOnFailure(_ body: () async throws -> Void) async throws {
