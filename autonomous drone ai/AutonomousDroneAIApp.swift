ab25503394806ae788837db05e7ff266f32810ea
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-20 01:24:47 +0000

    Removed tello controller for reimplementation.

--- autonomous drone ai/AutonomousDroneAIApp.swift
+++ autonomous drone ai/AutonomousDroneAIApp.swift
@@ -5,8 +5,6 @@ import SwiftUI
 @main
 struct AutonomousDroneAIApp: App {
     
-    let _telloController = TelloController()
-    
     var body: some Scene {
         Window(
             "Autonomous Drone AI",
@@ -16,26 +14,15 @@ struct AutonomousDroneAIApp: App {
                     let tello = Tello()
                     try! await tello.command()
                     try! await tello.streamOn()
-                    await _telloController.initializeTello(tello)
                 } .task(priority: .utility) {
                     for await notification in NotificationCenter.default.notifications(named: .GCControllerDidBecomeCurrent) {
                         let controller = notification.object as! GCController
                         guard let gamepad = controller.extendedGamepad else { continue }
-                        gamepad.buttonA.pressedChangedHandler = { [self] _, _, pressed in
+                        gamepad.buttonA.pressedChangedHandler = { _, _, pressed in
                             guard pressed else { return }
-                            Task(
-                                operation: {
-                                    await _telloController.buttonAPressed()
-                                }
-                            )
                         }
-                        gamepad.buttonB.pressedChangedHandler = { [self] _, _, pressed in
+                        gamepad.buttonB.pressedChangedHandler = { _, _, pressed in
                             guard pressed else { return }
-                            Task(
-                                operation: {
-                                    await _telloController.buttonBPressed()
-                                }
-                            )
                         }
                     }
                 } .task(priority: .utility) {
